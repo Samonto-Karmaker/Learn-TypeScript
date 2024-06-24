1912f02fa8202bdc682e5c337c9f26fac63a2d0a
Samanta Karmaker <samonto0726@gmail.com>
2024-06-24 23:22:41 +0600

    Intro to class in TS

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("This is a typescript file");
+class Person {
+    constructor(name, age) {
+        // any thing that is not initialized in the constructor should be initialized here
+        // of course, you can use readonly and ? if needed
+        this.id = 0;
+        this.name = name;
+        this.age = age;
+    }
+}
+let person = new Person('John', 30);
+console.log(person);
+console.log(person.id);
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,1 +1,15 @@
-console.log("This is a typescript file")
+class Person {
+    name: string;
+    age: number;
+    // any thing that is not initialized in the constructor should be initialized here
+    // of course, you can use readonly and ? if needed
+    id: number = 0;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let person = new Person('John', 30);
+console.log(person);
+console.log(person.id);
