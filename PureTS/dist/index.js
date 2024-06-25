0c9adf0a86650a6807d7918bee82508cb7095c5f
Samanta Karmaker <samonto0726@gmail.com>
2024-06-25 23:23:59 +0600

    Intro to Encapsulation in Typescript

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,13 +1,52 @@
 "use strict";
+// class Person {
+//     name: string;
+//     age: number;
+//     // any thing that is not initialized in the constructor should be initialized here
+//     // of course, you can use readonly and ? if needed
+//     id: number = 0;
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
 class Person {
-    constructor(name, age) {
-        // any thing that is not initialized in the constructor should be initialized here
-        // of course, you can use readonly and ? if needed
-        this.id = 0;
+    constructor(name, age, id) {
         this.name = name;
         this.age = age;
+        this.id = id;
+    }
+    // getters
+    get getName() {
+        return this.name;
+    }
+    get getAge() {
+        return this.age;
+    }
+    get getId() {
+        return this.id;
+    }
+    // setters: there should not be any return type for setters; not even void
+    set setName(name) {
+        this.name = name;
+    }
+    set setAge(age) {
+        this.age = age;
+    }
+    set setId(id) {
+        this.id = id;
+    }
+    // methods
+    printPerson() {
+        console.log(this.buildPerson());
+    }
+    buildPerson() {
+        return `Name: ${this.name}, Age: ${this.age}, ID: ${this.id}`;
     }
 }
-let person = new Person('John', 30);
+let person = new Person('John', 30, 1);
 console.log(person);
 console.log(person.id);
+person.printPerson();
+person.setName = 'Jane';
+console.log(person.getName);
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,15 +1,53 @@
+// class Person {
+//     name: string;
+//     age: number;
+//     // any thing that is not initialized in the constructor should be initialized here
+//     // of course, you can use readonly and ? if needed
+//     id: number = 0;
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
 class Person {
-    name: string;
-    age: number;
-    // any thing that is not initialized in the constructor should be initialized here
-    // of course, you can use readonly and ? if needed
-    id: number = 0;
-    constructor(name: string, age: number) {
+    constructor(private name: string, private age: number, public id: number) {}
+
+    // getters
+    get getName(): string {
+        return this.name;
+    }
+    get getAge(): number {
+        return this.age;
+    }
+    get getId(): number {
+        return this.id;
+    }
+
+    // setters: there should not be any return type for setters; not even void
+    set setName(name: string) {
         this.name = name;
+    }
+    set setAge(age: number) {
         this.age = age;
+    }
+    set setId(id: number) {
+        this.id = id;
+    }
+
+    // methods
+    public printPerson(): void {
+        console.log(this.buildPerson());
+    }
+    private buildPerson(): string {
+        return `Name: ${this.name}, Age: ${this.age}, ID: ${this.id}`;
     }
 }
 
-let person = new Person('John', 30);
+let person = new Person('John', 30, 1);
 console.log(person);
 console.log(person.id);
+
+person.printPerson();
+person.setName = 'Jane';
+console.log(person.getName);
